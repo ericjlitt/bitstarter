2b5527f929bfbeb39290e816cedd208e2606d2b1
Eric@AWS <ericlittler@yahoo.com>
2013-07-10 12:35:33 +0000

    Added new file grader.js

---
+++ grader.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+  + cheerio
+  + commander.js
+  + JSON
+*/
