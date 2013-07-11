f412e50b7f9f313f8aefd5107fddcb5ce46854f1
Eric@AWS <ericlittler@yahoo.com>
2013-07-11 13:19:15 +0000

    Accepts urls and files as input and processes correctly.

--- grader.js
+++ grader.js
@@ -8,13 +8,16 @@ References:
 
   + cheerio
   + commander.js
+  + restler
   + JSON
 */
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var restler = require('restler');
+//var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+//var URL_DEFAULT = "http://shielded-reef-2988.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -22,6 +25,19 @@ var assertFileExists = function(infile) {
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertUrlExists = function(inurl) {
+    var instr = inurl.toString();
+    /*instr = "http://shielded-reef-2988.herokuapp.com";
+    restler.get(instr).on('complete', function(result, response) {
+	fs.writeFile(__dirname + '/downloadedsite.html', result, function(err) {
+	console.log(instr);
+	if (err) throw err;
+	    console.log('It\'s saved!');
+	});
+    });*/
     return instr;
 };
 
@@ -35,6 +51,7 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+//    console.log($._root.children);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -51,9 +68,39 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program.option('c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT).option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT).parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_path>', 'Path to url', clone(assertUrlExists))
+	.parse(process.argv);
+
+    if(program.url){
+	console.log("Downloading and parsing %s", program.url);
+	var filename = "downloadedsite.html";
+	restler.get(program.url).on('complete', function(result, response){
+//	    console.log(program.url);
+//	    console.log(response.rawEncoded + "\n\n\n");
+	    fs.writeFile(filename, "hello");
+	    fs.writeFile(filename, response.rawEncoded);
+	});
+    }
+    else {
+	console.log("Parsing %s", program.file);
+	var filename = program.file;
+    }
+    fs.readFile(filename, "utf-8", function (err, data) {
+	if (err) throw err;
+//	console.log(data);
+    });
+    var checkJson = checkHtmlFile(filename, program.checks);
+//    console.log(filename);
     var outJson = JSON.stringify(checkJson, null, 4);
+    if(program.url){
+	/*fs.unlink(filename, function (err) {
+	    if (err) throw err;
+	    console.log('successfully deleted %s', filename);
+	});*/
+    }
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
